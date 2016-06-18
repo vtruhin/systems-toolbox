46f64dec681a5759159cb982e9203553e99cc375
Matthias Nehlsen <matthias.nehlsen@gmail.com>
2016-06-18 16:24:27 +0200

    systems-toolbox snapshot in mouse example

--- examples/trailing-mouse-pointer/project.clj
+++ examples/trailing-mouse-pointer/project.clj
@@ -3,22 +3,22 @@
   :url "https://github.com/matthiasn/systems-toolbox"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.7.228"]
-                 [org.clojure/core.async "0.2.374"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha7"]
+                 [org.clojure/clojurescript "1.9.76"]
+                 [org.clojure/core.async "0.2.385"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/tools.namespace "0.2.11"]
-                 [ch.qos.logback/logback-classic "1.1.6"]
+                 [ch.qos.logback/logback-classic "1.1.7"]
                  [hiccup "1.0.5"]
                  [hiccup-bridge "1.0.1"]
                  [garden "1.2.5" :exclusions [org.clojure/clojure]]
                  [clj-pid "0.1.2"]
-                 [matthiasn/systems-toolbox "0.5.14"]
-                 [matthiasn/systems-toolbox-ui "0.5.5"]
-                 [matthiasn/systems-toolbox-sente "0.5.12"]
-                 [matthiasn/systems-toolbox-metrics "0.5.2"]
+                 [matthiasn/systems-toolbox "0.6.1-SNAPSHOT"]
+                 [matthiasn/systems-toolbox-ui "0.6.1-SNAPSHOT"]
+                 [matthiasn/systems-toolbox-sente "0.6.1-SNAPSHOT"]
+                 [matthiasn/systems-toolbox-metrics "0.5.4"]
                  [incanter "1.5.6"]
-                 [clj-time "0.11.0"]]
+                 [clj-time "0.12.0"]]
 
   :source-paths ["src/cljc/" "src/clj/"]
 
@@ -27,8 +27,8 @@
   :main example.core
 
   :plugins [[lein-cljsbuild "1.1.3"]
-            [lein-figwheel "0.5.0-6"]
-            [lein-codox "0.9.4"]]
+            [lein-figwheel "0.5.4-3"]
+            [lein-codox "0.9.5"]]
 
   :figwheel {:server-port 3450
              :css-dirs    ["resources/public/css"]}
